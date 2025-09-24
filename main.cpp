16ae6ab2b8fb5f49ec388656c954d092ddb88c60
kezeyboye <keboye02@wsc.edu>
2025-09-24 09:36:23 -0500

    fixed some error

--- main.cpp
+++ main.cpp
@@ -14,9 +14,8 @@ int main() {
     string userName = "unknown";
      int userAnswer = 0;
     cout << "What's your name? :";
-    getline(&cin,[&]username) // collect the user detials as one variable
+    getline(cin,userName); // collect the user detials as one variable
     cin >> userName;   // reads input until the first space
-
     cout << "*  Welcome " << userName << " to the The Super Simple Math Tutor  *" << endl; //Greeting
      cout << endl;
     cout << "Sorry, but this version isnt set up fully yet. Please come back at V2" << endl;
@@ -27,8 +26,8 @@ int main() {
   cin >> secondNumber;
 
  int leftnum=rand() % 10+ 1;
- int rightnum rand() % 10+ 1;
- int opNum=rand() % 4 + 1
+ int rightnum =rand() % 10+ 1;
+ int opNum=rand() % 4 + 1;
  int correctAns;
  int userAns;
  int temp; //(used to make sure the left number is larger than the right for subtraction)\
